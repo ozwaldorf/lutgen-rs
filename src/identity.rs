4742ae6887ae80a0c56e6525dcf480ceb755b4cb
Sam Nystrom <sam@samnystrom.dev>
2023-06-21 11:58:46 -0400

    refactor: remove unnecessary floating-point casts
    The algorithm that generates the identity CLUT computes values using
    f64's because the result is a fraction in the range [0.0, 1.0) and then
    multiplies this to convert it to a u8. This can be simplified by
    multiplying by 255 first and then using integer division.
    
    This does not actually improve performance, I assume LLVM optimizes out
    the floating-point math.

--- src/identity.rs
+++ src/identity.rs
@@ -13,20 +13,20 @@ pub fn generate(level: u8) -> Image {
 
     let mut imgbuf = ImageBuffer::new(image_size, image_size);
 
-    let mut p = 0.0;
+    let mut p = 0u32;
     for blue in 0..cube_size {
         for green in 0..cube_size {
             for red in 0..cube_size {
-                let r = ((red as f64) / (cube_size - 1) as f64) * 255.0;
-                let g = ((green as f64) / (cube_size - 1) as f64) * 255.0;
-                let b = ((blue as f64) / (cube_size - 1) as f64) * 255.0;
+                let r = red * 255 / (cube_size - 1);
+                let g = green * 255 / (cube_size - 1);
+                let b = blue * 255 / (cube_size - 1);
                 let pixel = image::Rgb([r as u8, g as u8, b as u8]);
 
-                let x = p % image_size as f64;
-                let y = (p - x) / image_size as f64;
+                let x = p % image_size;
+                let y = (p - x) / image_size;
 
-                imgbuf.put_pixel(x as u32, y as u32, pixel);
-                p += 1.0;
+                imgbuf.put_pixel(x, y, pixel);
+                p += 1;
             }
         }
     }
