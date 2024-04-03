933696fc2a5a366d10c111596d83be97571fb015
ozwaldorf <self@ossian.dev>
2024-04-03 18:21:42 -0400

    feat: nix flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Lutgen";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs.lib) optionals;
+
+        craneLib = crane.lib.${system};
+
+        src = craneLib.path ./.;
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          buildInputs = [ ] ++ optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        lutgen =
+          craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+      in {
+        checks = {
+          fmt = craneLib.cargoFmt (commonArgs // { inherit cargoArtifacts; });
+          doc = craneLib.cargoDoc (commonArgs // { inherit cargoArtifacts; });
+          clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs =
+              "--all-targets --all-features -- -Dclippy::all -Dwarnings";
+          });
+          nextest =
+            craneLib.cargoNextest (commonArgs // { inherit cargoArtifacts; });
+        };
+        packages = { default = lutgen; };
+        apps.default = flake-utils.lib.mkApp { drv = lutgen; };
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+        };
+      });
+}
