8de756a96ca42dcd1a644cb48f0e89d0d4230738
ozwaldorf <self@ossian.dev>
2024-04-04 19:53:19 -0400

    flake: use nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -12,8 +12,16 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs.lib) optionals;
@@ -28,32 +36,44 @@
         };
 
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-        lutgen =
-          craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
-      in {
+        lutgen = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+      in
+      {
         checks = {
           fmt = craneLib.cargoFmt (commonArgs // { inherit cargoArtifacts; });
           doc = craneLib.cargoDoc (commonArgs // { inherit cargoArtifacts; });
-          clippy = craneLib.cargoClippy (commonArgs // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs =
-              "--all-targets --all-features -- -Dclippy::all -Dwarnings";
-          });
-          nextest = craneLib.cargoNextest (commonArgs // {
-            inherit cargoArtifacts;
-            cargoNextestExtraArgs = "--all-targets --all-features --all";
-          });
-          doctest = craneLib.cargoTest (commonArgs // {
-            inherit cargoArtifacts;
-            pnameSuffix = "-doctest";
-            cargoTextArgs = "--doc";
-          });
+          clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets --all-features -- -Dclippy::all -Dwarnings";
+            }
+          );
+          nextest = craneLib.cargoNextest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoNextestExtraArgs = "--all-targets --all-features --all";
+            }
+          );
+          doctest = craneLib.cargoTest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              pnameSuffix = "-doctest";
+              cargoTextArgs = "--doc";
+            }
+          );
         };
-        packages = { default = lutgen; };
+        packages = {
+          default = lutgen;
+        };
         apps.default = flake-utils.lib.mkApp { drv = lutgen; };
         devShells.default = craneLib.devShell {
           # Inherit inputs from checks.
           checks = self.checks.${system};
         };
-      });
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
