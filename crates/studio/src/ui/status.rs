274e23ab30243795b09980ec3e47eaef15225ce7
ozwaldorf <self@ossian.dev>
2025-08-07 15:57:12 -0400

    feat(studio): reactive statusline layout

--- crates/studio/src/ui/status.rs
+++ crates/studio/src/ui/status.rs
@@ -6,20 +6,37 @@ impl App {
     pub fn show_statusline(&self, ctx: &Context) {
         // statusline with events and other info
         egui::TopBottomPanel::bottom("statusline").show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                ui.label(&self.state.last_event);
-                ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                    if let Some(tex) = &self.state.image_texture {
+            if self.inline_layout {
+                ui.vertical_centered(|ui| {
+                    ui.add_space(5.);
+                    ui.label(&self.state.last_event);
+                    if let Some(tex) = &self.state.image_texture
+                        && let Some(path) = &self.state.current_image
+                    {
+                        ui.separator();
                         let [w, h] = tex.size();
-                        ui.label(format!("{w}x{h}"));
+                        let path = path.display().to_string();
+                        ui.label(format!("{path} ({w}x{h})"));
                     }
-                    ui.separator();
-                    if let Some(path) = &self.state.current_image {
-                        ui.label(path.display().to_string());
-                    }
-                    ui.separator();
+                    ui.add_space(5.);
                 });
-            });
+            } else {
+                ui.horizontal(|ui| {
+                    ui.label(&self.state.last_event);
+                    ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                        if let Some(tex) = &self.state.image_texture {
+                            let [w, h] = tex.size();
+                            ui.label(format!("{w}x{h}"));
+                            ui.separator();
+                        }
+                        if let Some(path) = &self.state.current_image {
+                            let path = path.display().to_string();
+                            ui.label(path);
+                            ui.separator();
+                        }
+                    });
+                });
+            }
         });
     }
 }
